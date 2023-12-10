8f09f4ac63e2a41b3b0cfab41a10cddd9bedde4b
Marina <rinanashaat64@gmail.com>
2023-12-10 18:25:38 +0200

    renaming the summation fn

--- ConsoleApplication.cpp
+++ ConsoleApplication.cpp
@@ -1,16 +1,13 @@
 # include <iostream>
 using namespace std;
-int my_sum(int a, int b);
+int sum(int a, int b);
 int main()
 {
 	cout << "Hello world" << endl;
-<<<<<<< HEAD
 	cout << "Let's try new concepts" << endl;
 	cout << "Learning git is fun!" << endl;
-=======
 }
-int my_sum(int a, int b)
+int sum(int a, int b)
 {
 	return a + b;
->>>>>>> math-feature
 }

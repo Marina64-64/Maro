ea927602982818b51eeecba3088b07c6f3f1fe35
Marina <rinanashaat64@gmail.com>
2023-12-14 01:34:14 +0200

    resolving conflict

--- ConsoleApplication.cpp
+++ ConsoleApplication.cpp
@@ -6,7 +6,7 @@ int main()
 	cout << "Hello world" << endl;
 	cout << "Let's try new concepts" << endl;
 	cout << "Learning git is fun!" << endl;
-	cout << my_sum(2, 4, 6);
+		cout << my_sum(2, 4, 6);
 }
 int my_sum(int a, int b, int c)
 {

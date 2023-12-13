e3ccbcb4d5b54589714374eb2c4a274f7b5de45e
Marina <rinanashaat64@gmail.com>
2023-12-14 01:36:11 +0200

    sum2 by master

--- ConsoleApplication.cpp
+++ ConsoleApplication.cpp
@@ -1,14 +1,14 @@
 # include <iostream>
 using namespace std;
-int my_sum(int a, int b, int c);
+int my_sum2(int a, int b, int c);
 int main()
 {
 	cout << "Hello world" << endl;
 	cout << "Let's try new concepts" << endl;
 	cout << "Learning git is fun!" << endl;
-	cout << my_sum(2, 4, 6);
+	cout << my_sum2(2, 4, 6);
 }
-int my_sum(int a, int b, int c)
+int my_sum2(int a, int b, int c)
 {
 	return a + b + c;
 }

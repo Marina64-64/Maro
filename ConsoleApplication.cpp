472ef66ecff1ffbf207beb771b1cec284bdd8f6b
Marina <rinanashaat64@gmail.com>
2023-12-14 01:49:23 +0200

    t3bt

--- ConsoleApplication.cpp
+++ ConsoleApplication.cpp
@@ -6,7 +6,7 @@ int main()
 	cout << "Hello world" << endl;
 	cout << "Let's try new concepts" << endl;
 	cout << "Learning git is fun!" << endl;
-		cout << my_sum3(2, 4, 6);
+	cout << my_sum3(2, 4, 6);
 }
 int my_sum3(int a, int b, int c)
 {

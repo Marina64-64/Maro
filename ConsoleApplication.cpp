3e98c96a07a5c68a7d9fdcbb75b3493af761fa03
Marina <rinanashaat64@gmail.com>
2023-12-14 01:24:14 +0200

    merging local with github

--- ConsoleApplication.cpp
+++ ConsoleApplication.cpp
@@ -1,23 +1,14 @@
 # include <iostream>
 using namespace std;
-<<<<<<< HEAD
 int my_sum(int a, int b, int c);
-=======
-int sum(int a, int b);
->>>>>>> math-feature
 int main()
 {
 	cout << "Hello world" << endl;
 	cout << "Let's try new concepts" << endl;
 	cout << "Learning git is fun!" << endl;
-<<<<<<< HEAD
 	cout << my_sum(2, 4, 6);
 }
 int my_sum(int a, int b, int c)
-=======
-}
-int sum(int a, int b)
->>>>>>> math-feature
 {
-	return a + b;
+	return a + b + c;
 }
